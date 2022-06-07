f4c0815ef3e4e3cc34bafcc122e7a7c7b1df5391
Iftakhar Husan <iftakharn19@gmail.com>
2022-06-07 16:36:11 +0600

    Add regular expression to check against file extension

--- __main__.py
+++ __main__.py
@@ -37,15 +37,16 @@ def get_file_path(
         return file_path
 
     # Prepare new file path
-    new_file_name = file_num_regex.subn(new_file_num, file_path.name, 1)
+    new_file_name = file_num_regex.subn(new_file_num, file_path.name, 1)[0]
     new_file_path = file_path.parent / new_file_name
     return new_file_path
 
 
 def filter_and_rename_files(
     parent_path: Path = parsed_args.path,
-    file_extensions: set[str] = set(parsed_args.file_extensions),
     is_recursive: bool = parsed_args.recursive,
+    file_extensions: set[str] = set(parsed_args.file_extensions),
+    regex_file_extensions: list[re.Pattern] = configs.REGEX_FILE_EXTENSIONS,
 ) -> None:
     max_file_number = 0
     files_to_rename: list[Path] = []
@@ -53,7 +54,11 @@ def filter_and_rename_files(
     for path in parent_path.iterdir():
         if path.is_dir() and is_recursive:
             # Recurse when path is a directory and is_recursive is True
-            filter_and_rename_files(path, file_extensions, is_recursive)
+            filter_and_rename_files(
+                parent_path=path,
+                file_extensions=file_extensions,
+                is_recursive=is_recursive,
+            )
             continue
 
         if path.is_file():
@@ -62,19 +67,30 @@ def filter_and_rename_files(
 
                 extensions = path.suffixes
                 for extension in extensions:
+                    is_rename_candidate = False
+
                     extension = extension[1:]
                     if extension in file_extensions:
+                        is_rename_candidate = True
+                    else:
+                        for regexp in regex_file_extensions:
+                            match = regexp.search(extension)
+                            if match:
+                                is_rename_candidate = True
+                                break
+
+                    if is_rename_candidate:
                         # Add path to rename candidate when file
                         # extension is in specified file extensions
                         files_to_rename.append(path)
                         max_file_number = max(file_num, max_file_number)
-
+                        break
             except ValueError:
                 pass
 
     for file in files_to_rename:
         new_file_path = get_file_path(file, max_file_number)
-        print("Renaming", file.name, "to", new_file_path.name)
+        print(file.name, "->", new_file_path.name)
         file.rename(new_file_path)
 
 
--- configs.py
+++ configs.py
@@ -3,7 +3,8 @@ import re
 
 RECURSIVE_RENAME = False
 
-FILE_EXTENSIONS = ["mp4", "mp3", "html", "zip"]
+FILE_EXTENSIONS: list[str] = ["mp4", "mp3", "html", "zip"]
+REGEX_FILE_EXTENSIONS: list[re.Pattern] = [re.compile(r"mp\d+")]
 
 FILE_NUM_GROUP_NAME = "file_num"
-FILE_NUM_REGEX = re.compile(rf"(?<=^)(?P<{FILE_NUM_GROUP_NAME}>\d+)(?=.*$)", re.M)
+FILE_NUM_REGEX = re.compile(rf"(?<=^)(?P<{FILE_NUM_GROUP_NAME}>\d+$)(?=.*$)", re.M)
