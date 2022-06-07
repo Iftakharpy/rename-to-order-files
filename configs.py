13da712312ea4cae2dcee9244fff1f7da74fcdac
Iftakhar Husan <iftakharn19@gmail.com>
2022-06-07 15:45:21 +0600

    add html and zip file to default file extensions

--- configs.py
+++ configs.py
@@ -3,7 +3,7 @@ import re
 
 RECURSIVE_RENAME = False
 
-FILE_EXTENSIONS = ["mp4", "mp3"]
+FILE_EXTENSIONS = ["mp4", "mp3", "html", "zip"]
 
 FILE_NUM_GROUP_NAME = "file_num"
 FILE_NUM_REGEX = re.compile(rf"(?<=^)(?P<{FILE_NUM_GROUP_NAME}>\d+)(?=.*$)", re.M)
