831a492ab4c6506790a378e844c02ec1342cb6c9
Iftakhar Husan <iftakharn19@gmail.com>
2022-06-07 14:16:31 +0600

    update regex and seperate label

--- configs.py
+++ configs.py
@@ -4,4 +4,6 @@ import re
 RECURSIVE_RENAME = False
 
 FILE_EXTENSIONS = ["mp4", "mp3"]
-FILE_NUM_REGEX = re.compile(r"^(?P<file_num>\d+).*$")
+
+FILE_NUM_GROUP_NAME = "file_num"
+FILE_NUM_REGEX = re.compile(rf"(?<=^)(?P<{FILE_NUM_GROUP_NAME}>\d+)(?=.*$)", re.M)
